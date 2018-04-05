ba8c4cbac2175dd595c3b4613106b7277beb954d
xumeng@gmail.com <xumeng@gmail.com>
2018-04-05 16:39:20 -0400

    3

---
+++ 305_NumberOfIslandII.py
@@ -0,0 +1,46 @@
+class UnionFind(object):
+    def __init__(self):
+        self.cnt = 0
+        self.dic = {}
+    
+    def add(self, i, j):
+        if (i, j) not in self.dic:
+            self.dic[(i, j)] = (i, j)
+            self.cnt += 1
+    
+    def find(self, p):
+        anc = self.dic[p]
+        while anc != self.dic[anc]:
+            anc = self.dic[self.dic[anc]]
+        return anc
+
+    def unite(self, p, q):
+        p_anc, q_anc = self.find(p), self.find(q)
+        if p_anc == q_anc:
+            return
+        else:
+            self.cnt -= 1
+            self.dic[p_anc] = q_anc
+
+class Solution(object):
+    def numIslands2(self, m, n, positions):
+        """
+        :type m: int
+        :type n: int
+        :type positions: List[List[int]]
+        :rtype: List[int] """
+        uf = UnionFind()
+        res = []
+
+        for i, j in positions:
+            uf.add(i, j)
+            for d, p in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                x, y = i+d, j+p
+                if (x, y) in uf.dic:
+                    uf.unite((i, j), (x, y))
+            res.append(uf.cnt)
+        return res
+
+p = [[0,1],[1,2],[2,1],[1,0],[0,2],[0,0],[1,1]]
+s = Solution()
+print(s.numIslands2(3, 3, p))
