0a7f08d895ec720a7cd4c4416b2a6c7ab6f2b58b
lonelyarcher <xumeng@gmail.com>
2018-04-12 21:51:33 -0400

    9

--- 213_HouseRobberII.py
+++ 213_HouseRobberII.py
@@ -7,13 +7,14 @@ class Solution(object):
         """
         if not nums:
             return 0
-        if len(nums) <= 2:
-            return max(nums)
-        s1 = [0, nums[0], nums[0]]
-        s2 = [0, 0, nums[1]]
-        for i in range(2, len(nums) - 1):
-            s1.append(max(nums[i] + s1[-2], s1[-1]))
-        for j in range(2, len(nums)):
+        if len(nums) == 1:
+            return nums[0]
+        s1 = [0, nums[0]]
+        s2 = [0, 0]
+        
+        for j in range(1, len(nums)):
+            if j < len(nums) - 1:
+                 s1.append(max(nums[j] + s1[-2], s1[-1]))
             s2.append(max(nums[j] + s2[-2], s2[-1]))
         return max(s1[-1], s2[-1])
 so = Solution()
