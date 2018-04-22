114f2fde88494bb071dc5d10f9521f03a677e3cd
lonelyarcher <xumeng@gmail.com>
2018-04-21 22:22:49 -0400

    autofill

--- trie_Autofill.py
+++ trie_Autofill.py
@@ -3,8 +3,33 @@ Implement a auto fill class. It will take a word dictionary for init. then it wi
 the word you are entered. You can understand this by looking at my code.
 '''
 class Solution():
-    pass
+    def __init__(self, words): # dict is not hashable because only immutable object could be hashed in Python
+        self.root = {}
+        self.rank = {w:0 for w in words}
+        for w in words:
+            cur = self.root
+            for c in w:
+                cur = cur.setdefault(c, {})
+            cur['value'] = w
+    def get_words(self, str):
+        cur = self.root
+        for c in str:
+            if c not in cur: return []
+            cur = cur[c]
+        suggestion = []
+        self.dfs(cur, suggestion)
+        return sorted(suggestion, key=lambda x: (-self.rank[x], x))
 
+    def dfs(self, cur, suggestion):
+        if 'value' in cur: 
+            suggestion.append(cur['value'])
+        for c in cur:
+            if c != 'value': self.dfs(cur[c], suggestion)
+
+    def pick(self, str):
+        self.rank[str] += 1
+
+            
 
 #test
 words = ['app', 'apple', 'application', 'appeal', 'ape', 'approve', 'apply', 'applicant', 'age', 'add', 'average', 'boy', 'bat']
