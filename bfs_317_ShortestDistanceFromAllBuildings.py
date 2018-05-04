28b51d491e6e220f7cccc5354e483a799bea9ca1
xumeng@gmail.com <xumeng@gmail.com>
2018-05-04 16:13:48 -0400

    shortest distance from buildings

--- bfs_317_ShortestDistanceFromAllBuildings.py
+++ bfs_317_ShortestDistanceFromAllBuildings.py
@@ -17,10 +17,36 @@ https://leetcode.com/problems/shortest-distance-from-all-buildings/
 思路： 我们用一个matrix来记录能到达i,j位置的building数和距离的和。然后对每一
 个grid值为1的点进行bfs就行了。
 '''
-
+from math import inf
 class Solution(object):
     def shortestDistance(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
+        if not grid or not grid[0]: return -1
+        matrix = {(i, j): [0, 0]   # key is coordinates, value is (count of buildings can be reached, total distance from all the buildings)
+            for i in range(len(grid)) 
+            for j in range(len(grid[0]))}
+        cnt = 0
+        for (i,j) in matrix:
+            if grid[i][j] == 1:
+                #BFS
+                queue = [(i,j,0)]
+                while queue:
+                    r, c, step = queue.pop(0)
+                    for m in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                        ne = (r + m[0], c + m[1])
+                        if ne in matrix and grid[ne[0]][ne[1]] == 0 and matrix[ne][0] == cnt:
+                            matrix[ne][0] += 1
+                            matrix[ne][1] += step + 1
+                            queue.append((ne[0], ne[1], step + 1))
+                cnt += 1
+
+        print(matrix)
+        return min(matrix[p][1] for p in matrix if matrix[p][0] == cnt)
+# test     
+grid = [[1,0,2,0,1],[0,0,0,0,0],[0,0,1,0,0]]
+so = Solution()
+a = so.shortestDistance(grid)
+print(a)
