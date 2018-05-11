4bb4d29f06daed72fd02b221ccb079dcec25ca3b
xumeng@gmail.com <xumeng@gmail.com>
2018-05-11 10:30:10 -0400

    finish the room clean robot

--- dfs_RoomCleanRobot.py
+++ dfs_RoomCleanRobot.py
@@ -4,13 +4,21 @@
 turn_left(k), turn_right(k)是旋转k*90度; 房间里可能有障碍物，机器人并不知道房间
 的布局，设计算法让扫地机器人清扫房间（走完房间每一格）。
 这道题主要是要把题目给抽象出来。简历坐标系，然后把调用API的函数给拉出来
+
 单独写。这样的话，题目本身其实就是一个dfs遍历matrix的题目了。
 # move(), turn_left(k), turn_right(k), clean()
 '''
 import copy
 class Robot(object):
-    mov_d = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)}
     
+    '''
+    题目隐藏了Robot的一种重要参数 abs direction, 即为当前robot朝那个方向:
+    我们定位为 0 x轴向右， j+1
+              1 y轴向下， i+1
+              2 x轴向左， j-1
+              3 y轴向上， i-1
+    '''
+    mov_d = {0: (0, 1), 1: (1, 0), 2: (0, -1), 3: (-1, 0)} 
     def __init__(self, x, y, dir):
         self.x = x
         self.y = y
@@ -52,26 +60,25 @@ class Robot(object):
 
     
 class Solution(object):
-
+    
     def robotClean(self, robot, room):
         self.robot = robot
         self.room = room
         self.visited = set()
         self.cleanable = len([ 1 for i in range(len(room)) for j in range(len(room[0])) if room[i][j] == 0 ])
-        self.dfs()
-        if len(self.visited) == self.cleanable:
-            print("Successfully clean the room")
-        else:
+        self.dfs()       
+        if len(self.visited) < self.cleanable:    
             self.robot.turn_right(1)
-            self.robotClean(self.robot, self.room)
-       
-
+            self.visited.clear()
+            print("couldn't move from the initial position, adjust the direction by right to ({},{},{})".format(self.robot.x, self.robot.y, self.robot.dir))
+            self.dfs()
 
     def dfs(self):
         if 0 <= self.robot.x < len(room) and 0 <= self.robot.y < len(room[0]) and room[self.robot.x][self.robot.y] == 0:
             self.robot.clean()
             self.visited.add((self.robot.x, self.robot.y))
             if len(self.visited) == self.cleanable:
+                print("Successfully clean the room, then return to the initail position")
                 return
             
             for d in [-1, 0, 1]:
@@ -90,6 +97,7 @@ room = [
 [1, 0, 1, 1]
 ]
 robot = Robot(0, 2, 0)
+print("initial robot ({},{},{})".format(robot.x, robot.y, robot.dir))
 s = Solution()
 s.robotClean(robot, room)
 
