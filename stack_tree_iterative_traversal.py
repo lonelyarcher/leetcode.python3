7abff720fb841f9fcc6c28cf471969777af2ef66
lonelyarcher <xumeng@gmail.com>
2018-05-21 22:43:36 -0400

    all iterative tree travesal

--- stack_tree_iterative_traversal.py
+++ stack_tree_iterative_traversal.py
@@ -4,7 +4,10 @@ class TreeNode(object):
         self.left = None
         self.right = None
 class Solution(object):
-    def inorder(self, root):
+
+    def inorder(self, root): 
+        # from root, go left, put root into stack
+        # when left to the end, no root then pop stack
         """
         :type root: TreeNode
         :rtype: List[int]
@@ -23,7 +26,9 @@ class Solution(object):
         return res
 
 
-        def preorder(self, root):
+    def preorder(self, root): 
+        # from root, travel root, put right into stack
+        # go left, when end pop the stack to right node.
         """
         :type root: TreeNode
         :rtype: List[int]
@@ -36,10 +41,31 @@ class Solution(object):
                 stack.append(root.right)
                 root = root.left
             else:
-                node = stack.pop()
-                
-                root = node.right
+                root = stack.pop()
+        return res
 
+    def postorder(self, root): 
+        # from root, go left, put the root into stack.
+        # keep record of last visit (in case don't repeat going right when root back from right)
+        # when left to end, peek the stack to go to its right if it has right and not visited ( right != pre) 
+        # if no right or already visit right, visit the root and record it as pre.
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        pre = None
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else: 
+                if stack[-1].right and pre != stack[-1].right:
+                    root = stack[-1].right
+                else:
+                    pre = stack.pop()
+                    res.append(pre.val)
         return res
 
 # Test
@@ -49,9 +75,11 @@ c = TreeNode(3)
 d = TreeNode(0)
 e = TreeNode(2.5)
 b.left, b.right = a, c
-a.right = d
+a.left = d
 c.left = e
 s = Solution()
 ans = s.inorder(b)
 print(ans)
+print(s.preorder(b))
+print(s.postorder(b))
 
