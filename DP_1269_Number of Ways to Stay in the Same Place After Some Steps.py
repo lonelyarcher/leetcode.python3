7fa85d1115ff33989bf7e1db775e3f941f7eab26
lonelyarcher <xumeng@gmail.com>
2019-11-25 18:09:19 -0500

    wip

--- DP_1269_Number of Ways to Stay in the Same Place After Some Steps.py
+++ DP_1269_Number of Ways to Stay in the Same Place After Some Steps.py
@@ -39,16 +39,27 @@ class Solution:
         state: i for step, j for position of arr
         dp[i][j] = dp[i - 1][j] + dp[i - 1][j + 1] + dp[i - 1][j - 1]
         rolling array can optimize the space to dp[j]
-        but in this question arrlen=10^6 >> steps=500, to optimization: the value of dp[0] is only related to 1 if step 1, 2 if step 2, 3 if step 3
-        n if step n
-        so if arrlen >> step, we only care about the step length of arr
+        but in this question arrlen=10^6 >> steps=500, to optimization, just look at the generated values from step 0, 1, 2, 3, 4, 5
+        step 0:  1, 0, 0, 0, 0, 0, 0
+        step 1:  1, 1, 0, 0, 0, 0, 0 
+        step 2:  2, 2, 1, 0, 0, 0, 0
+        step 3:  4, 5, 3, 1, 0, 0, 0
+        step 4:  9, 12,9, 4, 1, 0, 0
+        so at most step + 1 columns has value, and others are zero
+        we limits rolling array size to min(i + 1, arrLen), for i - 1, i , i + 1, each may has condition to be satisfied
+
+        
         """
         m = 1000000007
         dp = [1]
         for i in range(1, steps + 1):
             next = []
             for j in range(min(i + 1, arrLen)):
-                next.append((0 if j == 0 else dp[j - 1]) + (0 if j >= i else dp[j]) + (0 if j == arrLen - 1 or j >= i - 1 else dp[j + 1]))
+                next.append(
+                    (0 if j == 0 else dp[j - 1]) +  # j - 1, j can't be 0
+                    (0 if j >= i else dp[j]) + # j, j can't be i = step
+                    (0 if j == arrLen - 1 or j >= i - 1 else dp[j + 1]) #j + 1, j can't be great equals i - 1 and last of arrLen
+                )
             dp = next
         return dp[0] % m
 print(Solution().numWays(steps = 3, arrLen = 2)) #4
